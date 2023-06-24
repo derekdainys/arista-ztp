5eb4497453e2f51c775517e784938f1cad754756
Derek Dainys <46756382+derekdainys@users.noreply.github.com>
2023-06-23 19:27:39 -0500

    Add basic LLDP idea

---
+++ ztp.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+import subprocess
+import json
+import ipaddress
+
+lldpOutput = subprocess.getoutput("Cli -p 15 -c 'show lldp neighbors | json'")
+jsonOutput = json.loads(lldpOutput)
+
+for neighbor in jsonOutput["lldpNeighbors"]:
+    if neighbor["port"] == "Management1":
+        portOutput = neighbor["neighborPort"]
+
+        portNumber = int(list(filter(str.isdigit, portOutput))[0])
+        number = portNumber + 100
+        subnet = ""  # SPECIFY SUBNET TO BE GIVEN TO SWITCHES IN THE FOLLOWING FORMAT xxx.xxx.xxx. EX: 10.0.0.
+        netmask = ""  # SPECIFY NETMASK TO BE GIVEN TO THE SWITCHES IN THE FOLLOWING FORMAT xx EX: 24
+        ipAddress = f"{subnet}{number}/{netmask}"
+
+        network = ipaddress.ip_network(ipAddress, strict=False)
+        gateway = str(network[1])
+
+        config = f"""
+vrf instance MGMT
+!
+username admin secret admin
+!
+interface Management1
+vrf MGMT
+    ip address {ipAddress}
+!
+ip route vrf MGMT 0.0.0.0/0 {gateway}
+!
+management api http-commands
+    protocol http
+    no shutdown
+    !
+    vrf MGMT
+        no shutdown
+        protocol http
+    !
+!
+"""
+
+
+with open("/mnt/flash/startup-config", "w") as file:
+    file.write(config)
